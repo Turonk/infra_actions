fe8e200e9705ff2e0104122de9e3a685f605c18f
Andrew <gorlov.a@mail.ru>
2021-04-21 16:31:39 +0300

    add test url

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class StaticPagesURLTests(TestCase):
+    def setUp(self):
+        # Создаем неавторизованый клиент
+        self.guest_client = Client()
+
+    def test_about_url_exists_at_desired_location(self):
+        """Проверка доступности главной страницы."""
+        response = self.guest_client.get(reverse('infra_app:index'))
+        self.assertEqual(response.status_code, 200)
