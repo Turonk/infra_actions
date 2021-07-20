b2dd5350192eca51a4c329e76453bbf93e9fe8f7
LenaKiseleva <ellen91981@mail.ru>
2021-07-20 19:58:08 +0300

    Test commit

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #
 ]
 
 ROOT_URLCONF = 'infra_project.urls'
