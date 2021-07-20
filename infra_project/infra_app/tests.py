854c97d855a6100041c168dc2ee2908e683c7113
LenaKiseleva <ellen91981@mail.ru>
2021-07-20 21:40:45 +0300

    Commit

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -14,7 +14,7 @@ class StaticPagesURLTests(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_context(self):
-        """Проверка контекста страниц."""
+        """Проверка контекста страниц.."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
         response = self.guest_client.get('/second_page/')
--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
