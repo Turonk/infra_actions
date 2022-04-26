fad448e60f830793e2a87cddf741553400f47edd
Варвара <varvara.kireeva@gmail.com>
2022-04-26 17:41:53 +0200

    change text in views. checking deploy and secrets

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Давай workflow! Работай!')
 
 
 def second_page(request):
