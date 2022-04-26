e044894191af7aa3830e35da1d7495ac8cfabcda
Варвара <varvara.kireeva@gmail.com>
2022-04-26 17:43:46 +0200

    add deploy and secrets_2

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Давай workflow! Работай!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
