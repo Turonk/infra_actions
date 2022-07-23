ca271c2c2ead589ea61c2692d267bb7267a538ff
SergKorn <96118130+SergKorn@users.noreply.github.com>
2022-07-23 19:15:58 +0300

    18

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -11,16 +11,16 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get('index')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('second/')
+        response = self.guest_client.get('second_page')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get('index')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('second/')
+        response = self.guest_client.get('second_page')
         self.assertContains(response, 'А это вторая страница')
