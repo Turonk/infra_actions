49795791eddb2333ec1585b8c6019c26a76b8a2e
Никита Синицын <sinitsynik@yandex.ru>
2022-07-27 21:00:38 +0300

    test 5

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -18,7 +18,7 @@ class StaticPagesURLTests(TestCase):
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
         response = self.guest_client.get('/')
-        self.assertContains(response, 'У меня получилось!')
+        self.assertContains(response, 'ВСЕГО 6 Часов!')
 
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('ВСЕГО 6 Часов!')
 
 
 def second_page(request):
