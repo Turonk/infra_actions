91f886d1e9221ebed6fe786ee820869a7a1efe60
SergKorn <96118130+SergKorn@users.noreply.github.com>
2022-07-29 00:11:25 +0300

    20

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,9 +1,7 @@
 # infra_project/infra_app/tests.py
 
 from http import HTTPStatus
-
 from django.test import Client, TestCase
-
 
 class StaticPagesURLTests(TestCase):
     def setUp(self):
@@ -11,16 +9,17 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('index')
+        response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('second_page')
+        response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
-        response = self.guest_client.get('index')
+        response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('second_page')
+        response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница')
+
