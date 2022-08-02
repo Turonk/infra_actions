706a8f87701fe838044f8dd6871d7a1e7db633e0
Philipp Pyhonin <caveinfix@gmail.com>
2022-08-02 19:09:52 +0300

    first commit

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось, запуск workflow!')
 
 
 def second_page(request):
