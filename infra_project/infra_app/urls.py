73c895d52f5b9ff29f0c6df51d7658111be5f196
Boris Vinnik <vinniklawyer@yandex.ru>
2022-10-16 08:30:26 +0400

    final1

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'infra_app'
--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
 
-# urlpatterns = [
-#     path('', include('infra_app.urls', namespace='infra_app')),
-#     path('admin/', admin.site.urls),
-# ]
+urlpatterns = [
+    path('', include('infra_app.urls', namespace='infra_app')),
+    path('admin/', admin.site.urls),
+]
