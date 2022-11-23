a2f4c421c4b72b9f3a83903356f83ecd69898d1a
RuslanNakhmedzhanov <rus.nakhmedzhanov@gmail.com>
2022-11-23 19:33:01 +0300

    15

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'infra_app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('second/', views.second_page, name='second_page'),
+    path('/', views.index, name='index'),
+    path('/second/', views.second_page, name='second_page'),
 ]
