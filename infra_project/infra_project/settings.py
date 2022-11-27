5befcfb98701f95b45d2c7c9cb3b2611c5643b3a
RuslanNakhmedzhanov <rus.nakhmedzhanov@gmail.com>
2022-11-27 14:54:54 +0300

    allo

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '178.154.223.155'
+]
 
 
 # Application definition
