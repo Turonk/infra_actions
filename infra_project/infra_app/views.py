063c1787e690583c8718108c419a680f94da4564
RuslanNakhmedzhanov <rus.nakhmedzhanov@gmail.com>
2022-11-27 14:13:59 +0300

    48

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!!!')
 
 
 def second_page(request):
