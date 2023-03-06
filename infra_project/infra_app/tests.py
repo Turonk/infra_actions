fedd94510e0516901cc9049c4eac589f38854fe3
Marat Melkumyan <marat@MacBook-Pro-Marat.local>
2023-03-07 01:54:55 +0300

    3

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -21,4 +21,4 @@ class StaticPagesURLTests(TestCase):
         self.assertContains(response, 'У меня получилось!')
 
         response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 'А это вторая страница!')
+        self.assertContains(response, 'А это вторая страница')
