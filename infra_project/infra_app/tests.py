31be215ca6d55b371b08bad44df387c34099c08d
Marat Melkumyan <marat@MacBook-Pro-Marat.local>
2023-03-07 00:53:04 +0300

    3

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -10,10 +10,10 @@ class StaticPagesURLTests(TestCase):
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
         response = self.guest_client.get('/second_page/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
