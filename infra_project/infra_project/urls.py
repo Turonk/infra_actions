f28e175575a880d958cd5a6bbf5b2235af90b841
Виктор Антропов <antropovvic1997@yandex.ru>
2023-04-03 23:12:11 +0300

    The

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -13,9 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
