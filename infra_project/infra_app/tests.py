d14a2197b0dd21764e77cd5a9237ff0cb4f4c47b
sarymov <113104479+sarymov@users.noreply.github.com>
2023-04-27 18:35:35 +0300

    2nd tests

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -10,10 +10,10 @@ class StaticPagesURLTests(TestCase):
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, 200)
 
         response = self.guest_client.get('/second_page/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, 200)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
