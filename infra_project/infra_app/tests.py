edb121ae95c8f8ba20d6749ed939ae07d5f24981
sarymov <113104479+sarymov@users.noreply.github.com>
2023-04-27 18:50:37 +0300

    6

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -21,4 +21,4 @@ class StaticPagesURLTests(TestCase):
         self.assertContains(response, 'У меня получилось!')
 
         response = self.guest_client.get('/second_page/')
-        self.assertContains(response, HTTPStatus.NOT_FOUND)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
