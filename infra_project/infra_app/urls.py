02ac6bb2ff07f42edac2ca18d6b897b4bb909712
Василий Дутлов <dutlovvasya@yandex.ru>
2023-05-16 18:23:08 +0500

    debug urls

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'infra_app'
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('/second_page/', views.second_page, name='second_page'),
-
 ]
