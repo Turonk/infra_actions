200d7dec056bf42245da24d018191b34331835e9
dmay92 <unic1992@yandex.ru>
2023-05-21 18:59:25 +0300

    fix_tests

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -18,7 +18,7 @@ class StaticPagesURLTests(TestCase):
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
         response = self.guest_client.get('/')
-        self.assertContains(response, 'У меня получилось!')
+        self.assertContains(response.content, 'У меня получилось!')
 
         response = self.guest_client.get('/second/')
-        self.assertContains(response, 'А это вторая страница!')
+        self.assertContains(response.content, 'А это вторая страница!')
